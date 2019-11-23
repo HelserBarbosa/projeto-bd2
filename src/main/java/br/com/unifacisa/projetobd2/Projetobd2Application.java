ce7070c6eed8ba6263ec54e43c78f0301758f045
Helder Barbosa <helderlb@gmail.com>
2019-11-23 15:02:47 -0300

    teste

---
+++ src/main/java/br/com/unifacisa/projetobd2/Projetobd2Application.java
@@ -0,0 +1,13 @@
+package br.com.unifacisa.projetobd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetobd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetobd2Application.class, args);
+	}
+
+}
