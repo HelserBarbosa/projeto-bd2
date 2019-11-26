80cc8975ec60289b8e180b02d9bdb7d5f994893b
Jesse <jesseoito@gmail.com>
2019-11-26 18:15:26 -0300

    animal

--- src/main/java/br/com/unifacisa/projetobd2/daos/AnimalDAO.java
+++ src/main/java/br/com/unifacisa/projetobd2/daos/AnimalDAO.java
@@ -193,12 +193,11 @@ public class AnimalDAO {
 			statement.setString(6, animal.getRaca());
 			statement.setBigDecimal(7, animal.getPrecoCompra());
 			statement.setBigDecimal(8, animal.getPrecoVenda());
-
+			setGeneratedKey(animal, statement);
 			statement.execute();
-
 			statement.close();
 			connection.close();
-			return setGeneratedKey(animal, statement);
+			return animal;
 		} catch (SQLException e) {
 			throw new PetShopConnectionException(e.getMessage());
 		}
